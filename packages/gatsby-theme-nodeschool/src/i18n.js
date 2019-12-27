6324a96d076e80321dbf988583cea583b2c8b6a6
Petherson Moreno <pethermoreno@gmail.com>
2019-12-27 00:50:26 -0300

    feat: removed the need of ajax request to translation  - changed it to work with backend server side rendering

--- packages/gatsby-theme-nodeschool/src/i18n.js
+++ packages/gatsby-theme-nodeschool/src/i18n.js
@@ -1,17 +1,23 @@
 import i18n from "i18next";
-import Backend from "i18next-xhr-backend";
 import { initReactI18next } from "react-i18next";
 import defaultLanguage from './default-language.json';
+import translationEn from './locales/en/translation.json';
+import translationPtBr from './locales/pt-BR/translation.json';
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: {
+      en: {
+        translation: translationEn,
+      },
+      'pt-BR': {
+        translation: translationPtBr,
+      },
+    },
     lng: defaultLanguage,
     fallbackLng: `en`,
     debug: true,
