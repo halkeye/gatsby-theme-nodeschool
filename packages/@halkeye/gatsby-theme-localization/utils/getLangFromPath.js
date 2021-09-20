b6247b56527d4f55454b23dc3b55f09a748f5918
Gavin Mogan <git@gavinmogan.com>
2021-09-19 17:41:02 -0700

    fix what should be ignored files

--- packages/@halkeye/gatsby-theme-localization/utils/getLangFromPath.js
+++
@@ -1,6 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const getLangFromPath = (pathname) => {
-    const split = pathname.split('/').filter(nonEmpty => nonEmpty);
-    return split[0];
-};
-exports.default = getLangFromPath;
--- packages/@halkeye/gatsby-theme-localization/utils/preferDefault.js
+++
@@ -1,3 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-const preferDefault = (m) => (m && m.default) || m;
-exports.default = preferDefault;
